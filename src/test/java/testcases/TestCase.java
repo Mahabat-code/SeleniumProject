5b38d0931d086db9b6bf448b73c105149e3f69ba
Mahabat-code <129321113+Mahabat-code@users.noreply.github.com>
2023-03-30 20:36:03 -0400

    Initial commit

--- src/test/java/testcases/TestCase.java
+++ src/test/java/testcases/TestCase.java
@@ -7,8 +7,8 @@ import org.testng.annotations.Test;
 public class TestCase {
 
     @Test
-    public void startBrowser(){
-        System.setProperty("webdriver.chrome.driver","/Users/makhabatmb/IdeaProjects/SeleniumProject/src/test/resources/chromedriver");
+    public void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", "/Users/makhabatmb/IdeaProjects/SeleniumProject/src/test/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         ChromeDriver driver = new ChromeDriver(options);
@@ -16,12 +16,11 @@ public class TestCase {
         driver.get("https://www.nytimes.com");
 
     }
-}
 
-    String ssn_input = " //*[@id='ssn']";
     String id_input = "//*[@id='email']";
     String password_input = " //*[@id='password']";
     String remember_me_input = "//label[@for='auth-page-remember-me']";
     String forgot_password_input = "//a[@href='https://my-fork.com/password/reset']";
     String submit_input = "//button[@type='submit']";
 
+}
